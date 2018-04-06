ff1f4feee86e028bf7f9d2714444ee956a8490ed
shunsaku.nishio <ec2-user@ip-172-31-33-161.us-east-2.compute.internal>
2018-04-06 08:07:37 +0000

    kadai2-1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :require_user_logged_in
+  
 
   def create
     @micropost = Micropost.find(params[:micropost_id])
@@ -11,5 +12,12 @@ class FavoritesController < ApplicationController
     @micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(@micropost)
     redirect_back(fallback_location: root_url)
+  end
+  
+  
+  def favorite_microposts
+    @user = User.find(params[:id])
+    @favorite_microposts = @user.favorite_microposts.page(params[:page])
+    counts(@user)
   end
 end
